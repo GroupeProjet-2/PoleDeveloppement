d3894dc1bd5655db1753e7c64530804ba55d3e11
Tom Planche <tomplanche@icloud.com>
2022-12-13 08:41:29 +0100

    - .gitignore:   Added ignored files. - header.php:   Added the header with a theme switcher. - main.js:   - Theme switch handled here. - main.scss and all SCSS/CSS files:   Added styling. - index.php:   Added the home page.

---
+++ index.php
@@ -0,0 +1,53 @@
+<!-- // =============================================================================
+     //                             MADE BY TOM PLANCHE
+     //                            tomplanche@icloud.com
+     //                            github.com/tomPlanche
+     // =============================================================================-->
+
+<!doctype html>
+<html lang="fr">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+	    <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+
+
+        <meta charset="utf-8">
+        <meta name="authors" content="Mathis, Hériveau, Tom Montbord, Tom Planche">
+		<meta name="description" content="Proof Of Concept - SAE_3 Pole Développement">
+	    <meta name="viewport" content="width=device-width, height=device-height ,initial-scale=1.0">
+
+        <link rel="stylesheet" href="public/CSS/main.css">
+
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@300&display=swap" rel="stylesheet">
+
+        <title>Titre</title>
+    </head>
+
+    <body>
+        <noscript>You need to enable JavaScript to run this app.</noscript>
+
+        <?php
+            include('./src/header.php')
+        ?>
+
+        <main>
+            <?php
+                echo "Hello, World!";
+            ?>
+        </main>
+
+        <footer>
+
+	        <h1>Footer</h1>
+
+        </footer>
+
+    </body>
+
+    <script src="public/JS/main.js"></script>
+
+</html>
---
+++ src/header.php
@@ -0,0 +1,18 @@
+<header>
+    <h1>Auto-QCM</h1>
+
+    <div class="right">
+        <?php
+            session_start();
+            if (isset($_SESSION['user'])) {
+                echo '<a href="logout.php" class="link">Mon Compte</a>';
+            } else {
+                echo '<a href="login.php" class="link">Me connecter</a>';
+            }
+        ?>
+        <div id="themeSwitcherContainer">
+            <button type="button" id="themeSwitcher"></button>
+        </div>
+    </div>
+
+</header>
