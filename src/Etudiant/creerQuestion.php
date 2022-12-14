445c1eafcc60f084d0c0ecd233cecefe5686ff1e
Mathis heriveau <mheriveau@iutbayonne.univ-pau.fr>
2022-12-14 14:31:08 +0100

    Formulaire de création de question finalisé, il manquera le CSS

--- src/Etudiant/creerQuestion.php
+++ src/Etudiant/creerQuestion.php
@@ -9,19 +9,18 @@
     include '../BD/connexion_bd.php';
 
     // Récupération des données
+    $login = "bruyere"; //!!! A remplacer par la variable de session
+    $id_depot = 1;      //!!! A remplacer par la variable de session
+
+
     if (isset($_GET['Valider'])){
         $question = $_GET['Question'];
         $difficulte = $_GET['Difficulté'];
         $type = $_GET['Type'];
         $tag = $_GET['Tag'];
         $reponses = $_GET['Réponses'];
+        $bonneReponse = $_GET['bonneReponse'];
 
-        echo "Question : $question <br>";
-        echo "Difficulté : $difficulte <br>";
-        echo "Type : $type <br>";
-        echo "Tag : $tag <br>";
-        echo "Réponses : <br>";
-        var_dump($reponses);
 
         // Création de la question
         $sql = "INSERT INTO Question (LABEL, ETAT, TYPE, ID_UTILISATEUR, DIFFICULTE, ID_DEPOT) VALUES (:label, :etat, :type, :id_utilisateur, :difficulte, :id_depot)";
@@ -29,9 +28,9 @@
         $stmt->bindParam(':label', $question);
         $stmt->bindValue(':etat', 'A_VERIFIER');
         $stmt->bindParam(':type', $type);
-        $stmt->bindValue(':id_utilisateur', 'bruyere');
+        $stmt->bindValue(':id_utilisateur', $login);
         $stmt->bindParam(':difficulte', $difficulte);
-        $stmt->bindValue(':id_depot', '1');
+        $stmt->bindValue(':id_depot', $id_depot);
         $stmt->execute();
 
         echo "Question créée <br>";
@@ -40,16 +39,26 @@
         $id_question = $conn_bd->lastInsertId();
 
         // Création des réponses
-        $i = 0;
+        $i = 1;
         foreach ($reponses as $reponse) {
-            $sql = "INSERT INTO Reponse (LABEL, ETAT_VERITE, QUESTION_ID) VALUES (:label, :etat_verite, :question_id)";
-            $stmt = $conn_bd->prepare($sql);
-            $stmt->bindParam(':label', $reponse);
-            $stmt->bindValue(':etat_verite', $reponses[$i] == '1' ? 'VRAI' : 'FAUX');
-            $stmt->bindParam(':id_question', $id_question);
-            $stmt->execute();
+            if ($reponse != "") {
+                $sql = "INSERT INTO Reponse (LABEL, ETAT_VERITE, QUESTION_ID) VALUES (:label, :etat_verite, :question_id)";
+                $stmt = $conn_bd->prepare($sql);
+                $stmt->bindParam(':label', $reponse);
+                $etat = "FAUX";
+                foreach ($bonneReponse as $bonne) {
+                    if ($i == (int)$bonne) {
+                        $etat = "VRAI";
+                    }
+                }
+                $stmt->bindParam(':etat_verite', $etat);
+                $stmt->bindParam(':question_id', $id_question);
+                echo "Réponse : $reponse <br>";
+                $stmt->execute();
+            }
             $i++;
         }
+
 
         // Création du lien entre la question et le tag
         $sql = "INSERT INTO lier_tag_question (LABEL_TAG, ID_QUESTION) VALUES (:label_tag, :id_question)";
@@ -57,18 +66,7 @@
         $stmt->bindParam(':label_tag', $tag);
         $stmt->bindParam(':id_question', $id_question);
         $stmt->execute();
-
-        // Redirection vers la page de création de question
-        header('Location: creerQuestion.php');
-
-
-
     }
-
-
-
-
-
 ?>
 
 
@@ -109,13 +107,13 @@
         ?>
         <!-- Réponses -->
         <label for="Réponses">Vos réponses :</label>
-        <input type="checkbox" name="Réponses[]" value="1">
+        <input type="checkbox" name="bonneReponse[]" value="1">
         <input type="text" name="Réponses[]" id="Réponses">
-        <input type="checkbox" name="Réponses[]" value="2">
+        <input type="checkbox" name="bonneReponse[]" value="2">
         <input type="text" name="Réponses[]" id="Réponses">
-        <input type="checkbox" name="Réponses[]" value="3">
+        <input type="checkbox" name="bonneReponse[]" value="3">
         <input type="text" name="Réponses[]" id="Réponses">
-        <input type="checkbox" name="Réponses[]" value="4">
+        <input type="checkbox" name="bonneReponse[]" value="4">
         <input type="text" name="Réponses[]" id="Réponses">
         <!-- Tag -->
         <label for="Tag">Tag</label>
