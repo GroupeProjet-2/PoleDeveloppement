b6f60a5555d1c6401118e3e268bc6b3404e5f91a
tom montbord <montbordino@gmail.com>
2022-12-15 11:58:49 +0100

    finition du fichier de connexion sans le css

--- src/BD/connexion_utilisateur.php
+++ src/BD/connexion_utilisateur.php
@@ -14,7 +14,12 @@
     <br>
     <label for="password">Mot de passe</label>
     <input type="password" name="password" id="password">
+    <input type="checkbox" onclick="voirMdp()" class="check_box" id="checkboxMdp"> <label for="checkboxMdp"></label> <br>
+    <input type="checkbox" name="remember" id="remember" value="1">
+    <label for="checkboxMdp">se rappeler de moi</label>
+    <br>
     <input type="submit" value="Se connecter">
+
 </form>
 </body>
 </html>
@@ -34,7 +39,7 @@ if (isset($_POST['login']) && isset($_POST['password'])) {
     $password = hash('sha256',$_POST['password']);
 
     // On verifie si l'utilisateur existe dans la base de données
-    $query = $conn_bd->prepare("SELECT * FROM UTILISATEUR WHERE USER_LOGIN = '{$login}'");
+    $query = $conn_bd->prepare("SELECT * FROM UTILISATEUR WHERE USER_LOGIN = '{$login}' OR USER_EMAIL = '{$login}' LIMIT 1");
     $query->execute();
     $result = $query->fetchAll();
 
@@ -47,7 +52,11 @@ if (isset($_POST['login']) && isset($_POST['password'])) {
             // On crée la session et on l'enregistre dedans
             session_start();
             $_SESSION['utilisateur'] = new Utilisateur($login);
-            echo var_dump($_SESSION['utilisateur']);
+            if(isset($_POST['remember'])) {
+                setcookie('utilisateur', new Utilisateur($login), time() + 5*3600, null, null, false, true);
+            }
+
+            header('Location: ../../index.php');
 
         } else {
             // Le mot de passe est incorrect
@@ -60,5 +69,29 @@ if (isset($_POST['login']) && isset($_POST['password'])) {
 }
 ?>
 
+<script>
+    // Fonction pour afficher / cacher le mot de passe
+    inputMdp = document.getElementById("password"); // input du mot de passe
+    function voirMdp(){
+        inputMdp.type === "password" ? inputMdp.type = "text" : inputMdp.type = "password";
+    }
+</script>
+
+<style>
+    .check_box {
+        display:none;
+    }
+
+    .check_box + label{
+        background:url('../../public/images/eye-slash-solid.svg') no-repeat;
+        height: 16px;
+        width: 16px;
+        display:inline-block;
+    }
+
+    .check_box:checked + label{
+        background:url('../../public/images/eye-solid.svg') no-repeat;
+    }
+</style>
 
 
