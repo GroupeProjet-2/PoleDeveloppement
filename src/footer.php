d1e207e35516f7bf583e6176f59d39bb059aa025
Mathis heriveau <mheriveau@iutbayonne.univ-pau.fr>
2022-12-15 11:10:49 +0100

    Ajout du footer.php

---
+++ src/footer.php
@@ -0,0 +1,5 @@
+<footer>
+    <div class="footer">
+        <p>© 2020 - 2021 - Tous droits réservés - <a href="mentions-legales.php">Mentions légales</a> - <a href="conditions-generales.php">Conditions générales</a></p>
+    </div>
+</footer>
